f2184c5eb15927503eccb94b437adc9fc59761df
Tom Clesius <tomclesius@gmail.com>
2026-01-27 19:16:36 +0100

    fix: release scipt

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,6 @@ set -e
 
 BUMP_TYPE="${1:-patch}"
 
-uvx --from bump-my-version bump "$BUMP_TYPE" --commit --tag
+uvx --from bump-my-version bump-my-version bump "$BUMP_TYPE" --commit --tag
 git push origin main --tags
 gh release create "$(git describe --tags --abbrev=0)" --generate-notes
